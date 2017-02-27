6405edfe11ff00cbb4202cdd24ab20bd97719967
3ll3n <ellengrafton01@gmail.com>
2017-02-27 20:27:58 +0000

    added to layout

--- app/src/main/java/com/codeclan/example/stringsplit/StringSplitterActivity.java
+++ app/src/main/java/com/codeclan/example/stringsplit/StringSplitterActivity.java
@@ -1,10 +1,22 @@
 package com.codeclan.example.stringsplit;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 /**
  * Created by user on 27/02/2017.
  */
 
 public class StringSplitterActivity extends AppCompatActivity {
+
+    EditText words;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_string_splitter);
+
+        words = (EditText)findViewById(R.id.words);
+    }
 }
